315536146e0b3b27f3a681b955c016ed244a2365
Helmut Kemper <helmut.kemper@gmail.com>
2021-05-21 19:20:53 -0300

    working

--- funcSetGitCloneToBuildWithUserPassworh.go
+++ funcSetGitCloneToBuildWithUserPassworh.go
@@ -20,19 +20,3 @@ func (e *ContainerBuilder) SetGitCloneToBuildWithUserPassworh(url, user, passwor
 	e.gitData.user = user
 	e.gitData.password = password
 }
-
-// SetGitSshPassword (english):
-//
-// SetGitSshPassword (português): Define a senha da chave ssh para repositórios git privados.
-//
-//   Nota: O repositório pode ser definido pelos métodos SetGitCloneToBuild(), SetGitCloneToBuildWithPrivateSshKey(),
-//   SetGitCloneToBuildWithPrivateToken() e SetGitCloneToBuildWithUserPassworh()
-//
-//   SetPrivateRepositoryAutoConfig() copia as credencias do git contidas em ~/.ssh/id_rsa e as configurações de
-//   ~/.gitconfig
-//
-//   Caso o certificado ssh seja protegido com chave, ela pode ser definida com SetGitSshPassword()
-//
-func (e *ContainerBuilder) SetGitSshPassword(password string) {
-	e.gitData.password = password
-}
