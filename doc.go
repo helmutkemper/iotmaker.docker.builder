12395f0db9e5fe9c5207366978ef09aeebbb5f2a
Helmut Kemper <helmut.kemper@gmail.com>
2021-05-25 17:36:23 -0300

    documenting

--- doc.go
+++ doc.go
@@ -1,8 +1,10 @@
-// Package iotmakerdockerbuilder (english): Golang and Docker in a simple way.
+// Package iotmakerdockerbuilder
+//
+// English: Golang and Docker in a simple way.
 //
 // This package facilitates the use of docker containers by golang code
 //
-// Package iotmakerdockerbuilder (português): Golang e Docker de forma simples.
+// Português: Golang e Docker de forma simples.
 //
 // Este pacote facilita o uso de containers docker por código golang
 package iotmakerdockerbuilder
