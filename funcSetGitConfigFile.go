b0295e735019abb35dafbe391aeec75793da9e9b
Helmut Kemper <helmut.kemper@gmail.com>
2021-05-26 17:43:18 -0300

    documenting

--- funcSetGitConfigFile.go
+++ funcSetGitConfigFile.go
@@ -1,6 +1,24 @@
 package iotmakerdockerbuilder
 
-// SetGitConfigFile (english):
+// SetGitConfigFile (english): Defines the contents of the .gitconfig file
+//
+//     var err error
+//     var usr *user.User
+//     var path string
+//     var file []byte
+//     usr, err = user.Current()
+//     if err != nil {
+//       panic(err)
+//     }
+//
+//     path = filepath.Join(usr.HomeDir, ".gitconfig")
+//     file, err = ioutil.ReadFile(path)
+//     if err != nil {
+//       panic(err)
+//     }
+//
+//     var container = ContainerBuilder{}
+//     container.SetGitConfigFile(string(file))
 //
 // SetGitConfigFile (português): Define o conteúdo do arquivo .gitconfig
 //
