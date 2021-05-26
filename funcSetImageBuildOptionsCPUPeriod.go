f60ef31bc3602f7f92689c677ba9aafda2fa265b
Helmut Kemper <helmut.kemper@gmail.com>
2021-05-26 20:53:07 -0300

    documenting

--- funcSetImageBuildOptionsCPUPeriod.go
+++ funcSetImageBuildOptionsCPUPeriod.go
@@ -1,10 +1,14 @@
 package iotmakerdockerbuilder
 
-// SetImageBuildOptionsCPUPeriod (english): Specify the CPU CFS scheduler period, which is used alongside --cpu-quota.
+// SetImageBuildOptionsCPUPeriod
+//
+// English: Specify the CPU CFS scheduler period, which is used alongside --cpu-quota.
 // Defaults to 100000 microseconds (100 milliseconds). Most users do not change this from the default.
 // For most use-cases, --cpus is a more convenient alternative.
 //
-// SetImageBuildOptionsCPUPeriod (português):
+// Português: Especifique o período do agendador CFS da CPU, que é usado junto com --cpu-quota.
+// O padrão é 100.000 microssegundos (100 milissegundos). A maioria dos usuários não altera o padrão.
+// Para a maioria dos casos de uso, --cpus é uma alternativa mais conveniente.
 func (e *ContainerBuilder) SetImageBuildOptionsCPUPeriod(value int64) {
 	e.buildOptions.CPUPeriod = value
 }
--- funcSetImageBuildOptionsCacheFrom.go
+++ funcSetImageBuildOptionsCacheFrom.go
@@ -1,9 +1,12 @@
 package iotmakerdockerbuilder
 
-// SetImageBuildOptionsCacheFrom (english): CacheFrom specifies images that are used for matching cache.
+// SetImageBuildOptionsCacheFrom
+//
+// English: specifies images that are used for matching cache.
 // Images specified here do not need to have a valid parent chain to match cache.
 //
-// SetImageBuildOptionsCacheFrom (português):
+// Português: especifica imagens que são usadas para correspondência de cache.
+// As imagens especificadas aqui não precisam ter uma cadeia pai válida para corresponder a cache.
 func (e *ContainerBuilder) SetImageBuildOptionsCacheFrom(values []string) {
 	e.buildOptions.CacheFrom = values
 }
