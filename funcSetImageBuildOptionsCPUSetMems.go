6f57da85aa102a6a2ab4d6c7635ac04d2f5a5adc
Helmut Kemper <helmut.kemper@gmail.com>
2021-06-06 19:02:30 -0300

    documenting

--- funcSetImageBuildOptionsCPUSetMems.go
+++ funcSetImageBuildOptionsCPUSetMems.go
@@ -9,7 +9,12 @@ package iotmakerdockerbuilder
 // If you have four memory nodes on your system (0-3), use --cpuset-mems=0,1 then processes in your Docker container
 // will only use memory from the first two memory nodes.
 //
-// Português:
+// Português: Define memory node (MEMs) (--cpuset-mems)
+//
+// --cpuset-mems="" Memory nodes (MEMs) no qual permitir a execução (0-3, 0,1). Só funciona em sistemas NUMA.
+//
+// Se você tiver quatro nodes de memória em seu sistema (0-3), use --cpuset-mems=0,1 então, os processos em seu
+// container do Docker usarão apenas a memória dos dois primeiros nodes.
 func (e *ContainerBuilder) SetImageBuildOptionsCPUSetMems(value string) {
 	e.buildOptions.CPUSetMems = value
 }
