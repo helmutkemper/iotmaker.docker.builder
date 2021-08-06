54f30187fa713973890894569025f51911f2db91
Helmut Kemper <helmut.kemper@gmail.com>
2021-08-06 15:30:44 -0300

    working

--- funcAddImageBuildOptionsGitCredentials.go
+++ funcAddImageBuildOptionsGitCredentials.go
@@ -2,6 +2,7 @@ package iotmakerdockerbuilder
 
 import (
 	"github.com/docker/docker/api/types"
+	"log"
 )
 
 // addImageBuildOptionsGitCredentials
@@ -17,18 +18,22 @@ func (e *ContainerBuilder) addImageBuildOptionsGitCredentials() (buildOptions ty
 
 	if e.contentGitConfigFile != "" {
 		e.buildOptions.BuildArgs["GITCONFIG_FILE"] = &e.contentGitConfigFile
+		log.Printf("GITCONFIG_FILE: %v", e.contentGitConfigFile)
 	}
 
 	if e.contentKnownHostsFile != "" {
 		e.buildOptions.BuildArgs["KNOWN_HOSTS_FILE"] = &e.contentKnownHostsFile
+		log.Printf("KNOWN_HOSTS_FILE: %v", e.contentKnownHostsFile)
 	}
 
 	if e.contentIdRsaFile != "" {
 		e.buildOptions.BuildArgs["SSH_ID_RSA_FILE"] = &e.contentIdRsaFile
+		log.Printf("SSH_ID_RSA_FILE: %v", e.contentIdRsaFile)
 	}
 
 	if e.gitPathPrivateRepository != "" {
 		e.buildOptions.BuildArgs["GIT_PRIVATE_REPO"] = &e.gitPathPrivateRepository
+		log.Printf("GIT_PRIVATE_REPO: %v", e.gitPathPrivateRepository)
 	}
 
 	return
