0cf33c8c8572ce1e48ab86c79a3f6baab743890f
Helmut Kemper <helmut.kemper@gmail.com>
2021-08-18 14:46:23 -0300

    added function GetLastLineOccurrenceBySearchTextInsideContainerLog()

---
+++ funcGetLastLineOccurrenceBySearchTextInsideContainerLog.go
@@ -0,0 +1,29 @@
+package iotmakerdockerbuilder
+
+import (
+	"bytes"
+	"github.com/helmutkemper/util"
+)
+
+func (e *ContainerBuilder) GetLastLineOccurrenceBySearchTextInsideContainerLog(value string) (text string, contains bool, err error) {
+	var logs []byte
+	var lineList [][]byte
+	logs, err = e.GetContainerLog()
+	if err != nil {
+		util.TraceToLog()
+		return
+	}
+
+	logs = bytes.ReplaceAll(logs, []byte("\r"), []byte(""))
+	lineList = bytes.Split(logs, []byte("\n"))
+
+	for i := len(lineList) - 1; i >= 0; i -= 1 {
+		if bytes.Contains(lineList[i], []byte(value)) == true {
+			text = string(lineList[i])
+			contains = true
+			return
+		}
+	}
+
+	return
+}
