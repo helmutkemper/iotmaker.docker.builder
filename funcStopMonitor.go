12510648cf8a0c99cbb07297bf87a1a00df18689
Helmut Kemper <helmut.kemper@gmail.com>
2021-09-17 16:40:46 -0300

    bug fix

--- funcStopMonitor.go
+++ funcStopMonitor.go
@@ -1,11 +1,41 @@
 package iotmakerdockerbuilder
 
-func (e *ContainerBuilder) StopMonitor() {
+import "github.com/helmutkemper/util"
+
+func (e *ContainerBuilder) StopMonitor() (err error) {
 	if e.chaos.monitorStop == nil {
 		return
+	}
+
+	if e.containerID == "" {
+		err = e.GetIdByContainerName()
+		if err != nil {
+			util.TraceToLog()
+			return
+		}
+	}
+
+	e.chaos.linear = true
+
+	if e.chaos.containerPaused == true {
+		err = e.ContainerUnpause()
+		if err != nil {
+			util.TraceToLog()
+			return
+		}
+	}
+
+	if e.chaos.containerStopped == true {
+		err = e.ContainerRestart()
+		if err != nil {
+			util.TraceToLog()
+			return
+		}
 	}
 
 	if len(e.chaos.monitorStop) == 0 {
 		e.chaos.monitorStop <- struct{}{}
 	}
+
+	return
 }
