16a371eb6add3b2230b16d6d5b786bf67c6e928e
Helmut Kemper <helmut.kemper@gmail.com>
2021-09-27 13:18:19 -0300

    bugfix

--- funcContainerStatisticsOneShot.go
+++ funcContainerStatisticsOneShot.go
@@ -10,6 +10,11 @@ func (e *ContainerBuilder) ContainerStatisticsOneShot() (
 	err error,
 ) {
 
+	_, err = e.dockerSys.ContainerFindIdByName(e.containerName)
+	if err != nil {
+		return
+	}
+
 	statsRet, err = e.dockerSys.ContainerStatisticsOneShot(e.containerID)
 	if err != nil {
 		util.TraceToLog()
