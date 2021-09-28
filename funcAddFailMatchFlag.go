4d2ceed2c1c50d7393723fd70c35f27681955921
Helmut Kemper <helmut.kemper@gmail.com>
2021-09-27 21:56:54 -0300

    working

--- funcAddFailMatchFlag.go
+++ funcAddFailMatchFlag.go
@@ -1,5 +1,14 @@
 package iotmakerdockerbuilder
 
+// AddFailMatchFlag
+//
+// English: Error text searched for in the container's standard output.
+//   Input:
+//     value: Error text
+//
+// Português: Texto indicativo de erro procurado na saída padrão do container.
+//   Entrada:
+//     value: Texto indicativo de erro
 func (e *ContainerBuilder) AddFailMatchFlag(value string) {
 	if e.chaos.filterFail == nil {
 		e.chaos.filterFail = make([]LogFilter, 0)
