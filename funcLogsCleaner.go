63d81001a447d54c68df3f193014ff9544d94db9
Helmut Kemper <helmut.kemper@gmail.com>
2021-10-05 15:55:23 -0300

    bug fix

--- funcLogsCleaner.go
+++ funcLogsCleaner.go
@@ -1,6 +1,13 @@
 package iotmakerdockerbuilder
 
-import "bytes"
+import (
+	"bytes"
+	"github.com/helmutkemper/util"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"strconv"
+)
 
 func (e *ContainerBuilder) logsCleaner(logs []byte) [][]byte {
 
@@ -9,6 +16,22 @@ func (e *ContainerBuilder) logsCleaner(logs []byte) [][]byte {
 	// faz o log só lê a parte mais recente do mesmo
 	logs = logs[e.logsLastSize:]
 	e.logsLastSize = size
+
+	// todo: apagar - início
+	var dirList []fs.FileInfo
+	var err error
+	dirList, err = ioutil.ReadDir("./debug_log/")
+	if err != nil {
+		log.Printf("ioutil.ReadDir().error: %v", err.Error())
+		util.TraceToLog()
+	}
+	var totalOfFiles = strconv.Itoa(len(dirList))
+	err = ioutil.WriteFile("./debug_log/"+"log."+totalOfFiles+".log", logs, fs.ModePerm)
+	if err != nil {
+		log.Printf("ioutil.WriteFile().error: %v", err.Error())
+		util.TraceToLog()
+	}
+	// todo: apagar - fim
 
 	logs = bytes.ReplaceAll(logs, []byte("\r"), []byte(""))
 	return bytes.Split(logs, []byte("\n"))
