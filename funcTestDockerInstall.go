cbd152859dc2fae2c09549086798c0523fbb0294
Helmut Kemper <helmut.kemper@gmail.com>
2021-11-11 16:18:23 -0300

    working

---
+++ funcTestDockerInstall.go
@@ -0,0 +1,16 @@
+package iotmakerdockerbuilder
+
+import (
+	iotmakerdocker "github.com/helmutkemper/iotmaker.docker/v1.0.1"
+	"github.com/helmutkemper/util"
+)
+
+func (e ContainerBuilder) TestDockerInstall() (err error) {
+	e.dockerSys = iotmakerdocker.DockerSystem{}
+	err = e.dockerSys.Init()
+	if err != nil {
+		util.TraceToLog()
+		return
+	}
+	return
+}
