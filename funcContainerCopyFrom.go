421f478cd72501ba5b007ae2d13fe6902e757012
Helmut Kemper <helmut.kemper@gmail.com>
2021-12-17 14:52:51 -0300

    documenting

--- funcContainerCopyFrom.go
+++ funcContainerCopyFrom.go
@@ -6,6 +6,55 @@ import (
 	"io"
 )
 
+// ContainerCopyFrom
+//
+// Português: Oopia um arquivo contido no container para uma pasta local
+//   Entrada:
+//     sourcePath: caminho do arquivo no container
+//   Saída:
+//     reader: Reader para o arquivo contido no container. Ex.: fileInBytesArr, err = ioutil.ReadAll(reader)
+//     stats:  Informações do arquivo
+//     err:    Objeto padrão de error do Go
+//
+//   Exemplo:
+//      var reader io.Reader
+//	    var data []byte
+//	    reader, _, err = container.ContainerCopyFrom("/go/bin/golangci-lint")
+//	    if err != nil {
+//		    panic(err)
+//	    }
+//	    data, err = ioutil.ReadAll(reader)
+//	    if err != nil {
+//		    panic(err)
+//	    }
+//      err = ioutil.WriteFile("./alpineFiles/golangci-lint", data, 0777)
+//	    if err != nil {
+//		    panic(err)
+//	    }
+//
+// English: Copy a file contained in the container to a local folder
+//   Input:
+//     sourcePath: file path in container
+//   Output:
+//     reader: Reader for the file contained in the container. Eg: fileInBytesArr, err = ioutil.ReadAll(reader)
+//     stats:  file information
+//     err:    Go's default error object
+//
+//   Example
+//      var reader io.Reader
+//	    var data []byte
+//	    reader, _, err = container.ContainerCopyFrom("/go/bin/golangci-lint")
+//	    if err != nil {
+//		    panic(err)
+//	    }
+//	    data, err = ioutil.ReadAll(reader)
+//	    if err != nil {
+//		    panic(err)
+//	    }
+//      err = ioutil.WriteFile("./alpineFiles/golangci-lint", data, 0777)
+//	    if err != nil {
+//		    panic(err)
+//	    }
 func (e *ContainerBuilder) ContainerCopyFrom(
 	sourcePath string,
 ) (
